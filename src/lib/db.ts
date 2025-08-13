c57cc0492a8783f6bc88091fde52dfbe6375544b
Er-Ayush-DU <er.ayush336@gmail.com>
2025-08-13 23:31:11 +0530

    work on env , model and database connected

---
+++ src/lib/db.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+import mongoose from 'mongoose';
+
+export const dbConnect = async ()=>{
+  if (process.env.MongoDB_URI){
+    const mongoose = require('mongoose')
+  }
+try{
+  mongoose.connect(process.env.MONGODB_URI!);
+  const db = mongoose.connection;
+  db.on('connected', ()=>{
+    console.log('✅ Database connected successfully'); 
+  })
+  db.on('error', (error) => {
+    console.error('Database connection error:', error);
+    process.exit(1); // Exit the process if connection fails
+  });
+}
+catch (error) {
+  console.error('Database connection error:', error);
+  throw new Error('❌ Failed to connect to the database');   
+}
+}
